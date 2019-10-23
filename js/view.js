c819f04e6be640a5622fcf85ad4b0a164449828e
Martín Alejandro Barros <martinbarros86@gmail.com>
2019-10-23 02:37:45 -0300

    First try with createElement

--- js/view.js
+++ js/view.js
@@ -4,9 +4,30 @@ const view = (shop => {
         const hw = shop.testMethod(); // M<ethod from 'shop' module
         console.log(hw);
     }
+    const createElement = (element, id, className, text, type, placeholder, src) => {
+        const elm = document.createElement(element);
+        elm.id = id;
+        elm.className = className;
+        elm.innerText = text;
+        elm.type = type;
+        elm.placeholder = placeholder
+        elm.src = src;
+        return elm
+    }
+    const showElement = (elementId) => {
+        const id = document.getElementById(elementId);
+        const element1 = createElement('div', 'boxdiv', 'container')
+        const element = createElement('input','input_1', 'form-control', '', 'date', '', '');
+        element1.appendChild(element)
+        id.appendChild(element1);
+
+    }
     return {
+        showElement,
         testMethod
     }
 })(shop);
 
 view.testMethod();
+view.showElement('main')
+
