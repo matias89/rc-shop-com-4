59a2d6ced1c96fe92a53b65d77c38ae3219a3855
Giomanconi <Giomanconi@hotmail.com>
2019-10-23 20:31:27 -0300

    Format added

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,28 @@
 const shop = (() => {
-    const testMethod = () => {
+    
+    //Funcion para agregar los productos al carrito 
+    const addToCart = () => {
+        return 'Hello World!';
+    }
+
+    // Modifica los productos en el carrito
+    const modifyFromCart = () => {
+        return 'Hello World!';
+    }
+
+    // Borra productos en el carrito
+    const removeFromCart = () => {
+        return 'Hello World!';
+    }
+
+    // Verifica el proceso de finalizacion de compra
+    const verifyCheckout = () => {
         return 'Hello World!';
     }
     return {
-        testMethod
+        addToCart,
+        modifyFromCart,
+        removeFromCart,
+        verifyCheckout
     }
 })();
