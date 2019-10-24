01a3823e038c7553f8e1c51b48a8fffd47887343
Martín Alejandro Barros <martinbarros86@gmail.com>
2019-10-24 18:16:07 -0300

    tonteando con la tarea 7.comportamiento carrito

--- js/shop.js
+++ js/shop.js
@@ -2,7 +2,34 @@ const shop = (() => {
     const testMethod = () => {
         return 'Hello World!';
     }
+    const product = {
+        title: 'Leche',
+        id: '1',
+        size: '1 litro',
+        number: '1'
+    };
+    const addProduct = () => {
+        const formatToSave = JSON.stringify(product);
+        localStorage.setItem('Product', formatToSave);
+    }
+    const removeProduct = (product) => {
+        const info = localStorage.clear(product);
+    }
+    const editProduct = (key, newValue) => {
+        const info = localStorage.getItem('product');
+        const data = JSON.parse(info);
+        data.key = newValue;
+        localStorage.setItem( 'product', data.key = newValue )
+    }
     return {
-        testMethod
+        addProduct,
+        editProduct,
+        testMethod,
+        removeProduct
     }
 })();
+shop.addProduct();
+setTimeout(() => {
+    shop.removeProduct('product')
+    // shop.editProduct('title', 'pan');    
+}, 3000);
