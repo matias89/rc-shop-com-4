08b3a3084850c289f3c347afe68febfabeca7748
Martín Alejandro Barros <martinbarros86@gmail.com>
2019-10-23 21:15:29 -0300

    Second version

--- js/view.js
+++ js/view.js
@@ -4,10 +4,27 @@ const view = (shop => {
         const hw = shop.testMethod(); // M<ethod from 'shop' module
         console.log(hw);
     }
-    const showElement = (fatherId, tag, elementId, className, text, type, placeholder, src) => {
-        const id = document.getElementById(fatherId);
-        const element = shop.createElement(tag, elementId, className, text, type, placeholder, src);
-        id.appendChild(element);
+    const createElement = (element, id, className, html, type, placeholder, src) => {
+        const elm = document.createElement(element);
+        if (id) {
+            elm.id = id;
+        }
+        if (className) {
+            elm.className = className;
+        }
+        if (html) {
+            elm.innerHTML = html;
+        }
+        if (type) {
+            elm.type = type;
+        }
+        if (placeholder) {
+            elm.placeholder = placeholder;
+        }
+        if (src) {
+            elm.src = src;
+        }
+        return elm
     }
     return {
         showElement,
@@ -16,7 +33,6 @@ const view = (shop => {
 })(shop);
 
 view.testMethod();
-view.showElement('main', 'button','btn_1', 'btn btn-secondary m-4', 'Click me', 'button', '', '')
-view.showElement('main', 'p','p_1', 'text-success m-4', 'Hello friend...', '', '', '')
-view.showElement('main', 'img','img_1', '', '', '', '', 'https://www.gamepals.co/53-large_default/gafas-de-sol-new-balance-livianas-y-resistentes-al-impacto.jpg')
+view.showElement()
+
 
